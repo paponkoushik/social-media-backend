b527dcd1211dc767ac9c672abf914af5ec76899a
Koushik <paponkoushik@gmail.com>
2023-11-16 14:24:22 +0600

    JWT configured

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('jwt.auth')->get('/user', function (Request $request) {
     return $request->user();
 });
