224a58d096775f895ff507785d963903c42e8873
Koushik <paponkoushik@gmail.com>
2023-11-21 17:16:02 +0600

    api response little changed

--- app/Http/Resources/UserInfo/FollowerFollowingResource.php
+++ app/Http/Resources/UserInfo/FollowerFollowingResource.php
@@ -9,10 +9,13 @@ class FollowerFollowingResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $isFollowedByAuthUser = auth()->user()->following->contains($this->id);
+
         return [
             'id' => $this->id,
             'username' => $this->username,
             'email' => $this->email,
+            'is_followed_by_auth_user' => $isFollowedByAuthUser,
             'created_at' => $this->created_at?->format('F Y'),
         ];
     }
